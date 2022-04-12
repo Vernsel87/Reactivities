9f4fe1c895b9a50736320fcc4118391fb96e4baf
Verney Sandriman <verney@Verneys-Mac-mini.local>
2022-04-12 21:24:24 +0100

    Finished section 7

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import {  Container, } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
+import { useStore } from "../stores/store";
+import { observer } from "mobx-react-lite";
+
+function App() {
+  const {activityStore} = useStore();
+
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore]);
+
+  if (activityStore.loadingInitial) return <LoadingComponent content="Loading activities ..." />;
+
+  return (
+    <>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard/>
+      </Container>
+    </>
+  );
+}
+
+export default observer(App);
---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { useStore } from "../stores/store";
+
+export default function NavBar() {
+
+  const {activityStore} = useStore();
+
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img src="/assets/logo.png" alt="logo" style={{marginRight: 10}}/>
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities" />
+        <Menu.Item>
+          <Button positive content="Create Activity" onClick={()=>activityStore.openForm()} />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+}
